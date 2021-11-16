c8faf2b9e9b0515454eee3e229dfa1415315329c
MirInjamamul <injamul120221@gmail.com>
2021-11-16 23:32:09 +0600

    Added dashboard Design scratch . Not Completed yet

--- app/src/main/java/com/example/safetyaid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/safetyaid/ui/dashboard/DashboardFragment.java
@@ -1,13 +1,16 @@
 package com.example.safetyaid.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -15,32 +18,38 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.safetyaid.R;
 import com.example.safetyaid.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements View.OnClickListener {
 
-    private DashboardViewModel dashboardViewModel;
-    private FragmentDashboardBinding binding;
+    private static final String TAG = "Dashboard Fragment";
+    private View dashboard_view;
+    private CardView panic_trigger_card_view, cardView2, cardView3, cardView4, cardView5, cardView6;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        dashboard_view = inflater.inflate(R.layout.fragment_dashboard, null);
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        panic_trigger_card_view = (CardView) dashboard_view.findViewById(R.id.panic_trigger_card_view);
+
+        panic_trigger_card_view.setOnClickListener(this);
+
+        return dashboard_view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+    }
+
+    @Override
+    public void onClick(final View v) { //check for what button is pressed
+        switch (v.getId()) {
+            case R.id.panic_trigger_card_view:
+                Toast.makeText(getActivity(),"Coming Soon !!!",Toast.LENGTH_LONG).show();
+                break;
+            default:
+
+                break;
+        }
     }
 }
